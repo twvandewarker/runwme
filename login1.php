c29719d19372bd626990a5ae76756994e790dfe1
Rhea Braun <rhea.m.braun@gmail.com>
2019-05-09 18:21:37 -0400

    more work on profile page

--- login1.php
+++ login1.php
@@ -1,5 +1,59 @@
 <?php
-echo '
+
+  $user = isset($user) ? $user : $_POST["User"];
+  $pass = isset($pass) ? $pass : $_POST["Pass"];
+
+  if (!$user or !$pass) {
+
+    echo '
+    <html>
+      <head>
+        <title>Empty fields</title>
+      </head>
+      <body bgcolo="white" text="black">
+        <p>Empty Field. Please try again. Redirecting you back.</p> 
+        <meta http-equiv="refresh" content="3; url=index.php" />
+      </body>
+    </html>
+    ';
+
+  } else {
+
+    include ("readDb.php");
+
+    if ($found == 0) {
+
+      echo '
+      <html>
+        <head>
+          <title> Username does not exist </title>
+        </head>
+        <body bgcolor="white" text="black">
+          <p>Username not found. Please try again. Redirecting you back.</p>
+          <meta http-equiv="refresh" content="3; url=index.php" />
+        </body>
+      </html>
+      ';
+
+    } else {
+      
+      if ($pass != $passdB) {
+
+        echo '
+        <html>
+          <head>
+            <title>Incorrect Password</title>
+          </head>
+          <body bgcolor="white" text="black">
+            <p>Wrong password. Please try again. Redirecting you back.</p>
+            <meta http-equiv="refresh" content="3; url=index.php" />
+          </body>
+        </html>
+        ';
+
+      } else {
+
+        echo '
           <html>
             <title> "The HandyRides - ' . $row[fName] . '\'s profile."</title>
           <body bgcolor="white" text="black">
@@ -67,4 +121,11 @@ echo '
       </html>
       ';
 
+      }
+    }
+  }
+
 ?>
+
+
+
