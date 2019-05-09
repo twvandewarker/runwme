98b78dff38dd4e042671b0c172bd659ee58b1628
Travis Vandewarker <twvandewarker@gmail.com>
2019-05-09 00:16:09 -0400

    fixed index

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="style.css" />
-        <title> runw/me </title>
+        <title>runw/me</title>
     </head>
 
     <body>
       <right>
         <nobr>
-          <a href="../signup_page"> Sign Up </a>
-          &emsp
-          a href="login_page"> Log In </a>
+          <a href="../signup_page">Sign Up</a>
+          <pre>&emsp</pre>
+          <a href="login_page">Log In</a>
         </nobr>
       </right>
         
