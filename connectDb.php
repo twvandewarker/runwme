119b69b41c44f5168c853268fff09a319d20ab86
Travis Vandewarker <twvandewarker@gmail.com>
2019-05-08 22:13:36 -0400

    Edit connectdb.php

--- connectDb.php
+++ connectDb.php
@@ -2,9 +2,9 @@
 
   // Connect to the database for the entry of the CSV stuff into the database.
   $dbhost = "localhost";
-  $dbuser = "twv_travisv";     // CHANGE IT TO YOUR DATABASE USER NAME
-  $dbpass = "twv_password";            // CHANGE IT TO YOUR DATABASE PASSWORD
-  $dbname = "twv_lab2";     // CHANGE IT TO YOUR DATABASE NAME
+  $dbuser = "twv_travis";     // CHANGE IT TO YOUR DATABASE USER NAME
+  $dbpass = "twv_4rwZpa45";   // CHANGE IT TO YOUR DATABASE PASSWORD
+  $dbname = "twv_runwme";     // CHANGE IT TO YOUR DATABASE NAME
   $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ("Error connecting to mysql");
       
 ?>
