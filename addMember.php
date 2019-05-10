d32e1062e23deaa66b6dbc34f212943dd0af5a54
Travis Vandewarker <twvandewarker@gmail.com>
2019-05-09 22:52:35 -0400

    db

--- addMember.php
+++ addMember.php
@@ -29,9 +29,9 @@
        $hash = password_hash($pass, PASSWORD_DEFAULT);
 
 echo "Hashed password";
-
-include ("connectDb.php");
 /*
+include ("connectDb.php");
+
        $sql = "INSERT INTO runwme_users (username, real_name, password_hash, run_walk_bike, times_available, location_x, location_y, bio, contact_info) VALUES ('$user' ,'$name', '$hash', '$mode', '$avail', '$xcoords', '$ycoords', '$bio', '$info')";
 
       $result = mysqli_query($conn, $sql);
