9e6d97fb2d1919326e999e68eb256f72ff6d8173
Rhea Braun <rhea.m.braun@gmail.com>
2019-05-10 04:58:49 -0400

    some fixes

--- addMember.php
+++ addMember.php
@@ -28,10 +28,10 @@
         $hash = password_hash($pass, PASSWORD_DEFAULT);
 
         include ("connectDb.php");
-        include("transformations.php");
+        //include("transformations.php");
         echo ''. $avail .' '. $mode .'';
-        //$times = TimeToNum($avail);
-        //$modes = ModeToNum($mode);
+        $times = 7;
+        $modes = 63;
         $sql = "INSERT INTO runwme_users (username, real_name, password_hash, run_walk_bike, times_available, location_x, location_y, bio, contact_info) VALUES ('$user' ,'$name', '$hash', '$mode', '$avail', '$xcoords', '$ycoords', '$bio', '$info')";
 
         $result = mysqli_query($conn, $sql);
