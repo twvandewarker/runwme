e9d008e6b1f2224a4f0f29d3c6030dd8eec05eb8
Rhea Braun <rhea.m.braun@gmail.com>
2019-05-10 04:15:31 -0400

    removed statements

--- getMatches.php
+++ getMatches.php
@@ -17,7 +17,6 @@
 
           $sqlt2 = "SELECT * FROM runwme_users";
 
-        echo "Finished setting sqlt\n";
 
         // Again, Send the request
         $all_results = mysqli_query($conn, $sqlt2);
@@ -28,11 +27,9 @@
         else {
             $found2 = number_format(mysqli_num_rows($all_results));
             while ($arow = mysqli_fetch_array($all_results)){
-                echo $arow["username"];
                 if (($row["times_available"] & $arow["times_available"]) and
                 ($row["run_walk_bike"] & $arow["run_walk_bike"])){
                     array_push($matches, $arow["username"]);
-                    echo 'new match founbd';
                 }
                 
             }
