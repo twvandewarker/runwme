083fbad6e3faa2b2b91a22298877d3d0ac351446
Rhea Braun <rhea.m.braun@gmail.com>
2019-05-10 04:09:30 -0400

    fixed addmember for thig

--- addMember.php
+++ addMember.php
@@ -28,8 +28,10 @@
         $hash = password_hash($pass, PASSWORD_DEFAULT);
 
         include ("connectDb.php");
-
-        $sql = "INSERT INTO runwme_users (username, real_name, password_hash, run_walk_bike, times_available, location_x, location_y, bio, contact_info) VALUES ('$user' ,'$name', '$hash', '$mode', '$avail', '$xcoords', '$ycoords', '$bio', '$info')";
+        include("transformations.php");
+        $times = TimeToNum($avail);
+        $modes = ModeToNum($mode);
+        $sql = "INSERT INTO runwme_users (username, real_name, password_hash, run_walk_bike, times_available, location_x, location_y, bio, contact_info) VALUES ('$user' ,'$name', '$hash', '$modes', '$times', '$xcoords', '$ycoords', '$bio', '$info')";
 
         $result = mysqli_query($conn, $sql);
 
