0cbc76b6f4ed9788bf0e8b03efca7c478e6a55d9
Rhea Braun <rhea.m.braun@gmail.com>
2019-05-10 02:37:20 -0400

    test 1 to fix login

--- login.php
+++ login.php
@@ -38,16 +38,17 @@
     }
     else {
 echo "about to test password <br />";
-      /*if (password_verify($pass, $hash_returned) {
-        include("profile.php");
-      } */
+      if (password_verify($pass, $hash_returned) {
+        //include("profile.php");
+        echo "good pass";
+      } 
 
-      //else {
+      else {
         echo '
             <p>Wrong password. Please try again. Redirecting you back.</p>
             <meta http-equiv="refresh" content="3; url=login.html" />
         ';
-      //}
+      }
     }
   }
 
