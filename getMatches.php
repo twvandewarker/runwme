19aecbad724799bb5dfb1502e464e496237fdd60
Rhea Braun <rhea.m.braun@gmail.com>
2019-05-10 03:53:45 -0400

    mood

--- getMatches.php
+++ getMatches.php
@@ -17,7 +17,7 @@ echo "Finished setting sqlt\n";
     $matches = array();
   	$found = number_format(mysqli_num_rows($user_result));
   	$urow = mysqli_fetch_array($user_result);
-    /*
+    
     if ($found) {
 
           $sqlt2 = "SELECT * FROM runwme_users";
@@ -26,7 +26,7 @@ echo "Finished setting sqlt\n";
 
         // Again, Send the request
         $all_results = mysqli_query($conn, $sqlt2);
-                  
+               /*   
         if (!$all_results) {
             die("SQL Error Getting User Information: " . mysqli_error($conn));
         }
@@ -34,16 +34,15 @@ echo "Finished setting sqlt\n";
             $found2 = number_format(mysqli_num_rows($all_results));
             while ($row = mysqli_fetch_array($all_results)){
                 if (($urow["times_available"] & $row["times_available"]) &&
-                ($urow["run_walk_bike"] & $row["run_walk_bike"]) &&
-                ($locatonsmatch){
+                ($urow["run_walk_bike"] & $row["run_walk_bike"])){
                     array_push($matches, $row["username"]);
                 }
                 
             }
       
         }
-        
-    } */
+        */
+    } 
     mysqli_close($conn); 
   }
     
