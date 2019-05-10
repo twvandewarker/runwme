41bcddd798c9393a2e1983f7418563684b2da12f
Travis Vandewarker <twvandewarker@gmail.com>
2019-05-09 23:30:58 -0400

    db

--- connectDb.php
+++ connectDb.php
@@ -6,8 +6,11 @@
   $dbpass = "twv_4rwZpa45";
   $dbname = "twv_runwme";
 
-  $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ("Error connecting to mysql");
+  $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
+  if (mysqli_connect_errno()) {
+     echo "Failed to connect to MySQL: " / mysqli_connect_error();
+     }
+     
 echo "Finished db connect";
-echo $conn;
 ?>
