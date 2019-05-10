8246f5aedf9fab8526a6ae74209bb6731ee46183
Rhea Braun <rhea.m.braun@gmail.com>
2019-05-09 22:32:04 -0400

    started on adding matches page

--- matches.php
+++ matches.php
@@ -2,7 +2,7 @@
 echo '
 <html>
 <head>
-    <title> "runwme ' . $row["real_name"] . '"</title>
+    <title> "runwme ' . $row["real_name"] . ' matches"</title>
 	<link rel="stylesheet" type="text/css" href="style.css" />
    
     </head>
@@ -10,32 +10,34 @@ echo '
             <ul class="menu">
               
               <li><a href="profile.php">Profile</a></li>
-              <li><a href="#contact">Matches</a></li>
-              <li><a href="#about">Besties</a></li>
+              <li><a href="matches.php">Matches</a></li>
+              <li><a href="besties.php">Besties</a></li>
               <img src ="logo.png" />
             </ul>
           <div>
             <center>
               <br /> <br /> 
-              <p style="font-size:120%">Hi ' . $row["real_name"] . ', welcome back! Here is your profile:</p>
+              <p style="font-size:120%">Hi ' . $row["real_name"] . ', welcome back! Here some potential friends to run with:</p>
               <br />
-                <div class="prof_box">
+                <div class="tiles">
+                ';
+                
+            include("getMatches.php")
+            while ($row = mysql_fetch_assoc($result)) {
+                echo '<div>
                 <img src ="runman.png" align="center" style="height:20%"/>
                 <p style="font-size:150%" align=center> ' . $row["real_name"] . '</p>
                 <p style="font-size:80%" align=center> ' . $row["username"] . '</p>
                 <p style="font-size:80%" align=center> ' . $row["location_x"] . ',  ' . $row["location_y"] . '</p>
                 <p style="font-size:80%" align=center> ' . $row["contact_info"] . '</p>
                 <p align=center> Bio: ' . $row["bio"] . '</p> 
+                <div>'
+            }  
+            echo '
                 </div>
 <br />
             <br /><br />
 
-            <h3>Update Profile</h3>
-            
-            <br /><br />
-            
-            <p> Want to leave runwme? Shoot us an email, and we\'ll take you off our database. </p>
-            
             <br />
 </div>
             <a href="index.html" style="vertical-align: baseline;"> Return to Homepage </a>
