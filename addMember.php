d49693f90c4657a820a32b3f66fbab77fe3d1050
Travis Vandewarker <twvandewarker@gmail.com>
2019-05-10 00:29:18 -0400

    db

--- addMember.php
+++ addMember.php
@@ -1,6 +1,3 @@
-<?php
-
-echo '
 <!DOCTYPE html>
 <html>
   <head>
@@ -8,8 +5,8 @@ echo '
   </head>
   
   <body>
-';
 
+<?php
   // variables from form
   $user = $_POST["User"];
   $pass = $_POST["Pass"];
@@ -29,41 +26,38 @@ echo "Started php, made variables from post";
 
 echo "Finished readDb.php";
 
-  //add users now
-  if ($found == 0) {
-       // Hash password
-       $hash = password_hash($pass, PASSWORD_DEFAULT);
+     //add users now
+     if ($found == 0) {
+        // Hash password
+        $hash = password_hash($pass, PASSWORD_DEFAULT);
 
-echo "Hashed password";
+        echo "Hashed password";
 
-include ("connectDb.php");
+        include ("connectDb.php");
 
-       $sql = "INSERT INTO runwme_users (username, real_name, password_hash, run_walk_bike, times_available, location_x, location_y, bio, contact_info) VALUES ('$user' ,'$name', '$hash', '$mode', '$avail', '$xcoords', '$ycoords', '$bio', '$info')";
+        $sql = "INSERT INTO runwme_users (username, real_name, password_hash, run_walk_bike, times_available, location_x, location_y, bio, contact_info) VALUES ('$user' ,'$name', '$hash', '$mode', '$avail', '$xcoords', '$ycoords', '$bio', '$info')";
 
-      $result = mysqli_query($conn, $sql);
+        $result = mysqli_query($conn, $sql);
 
-      if ($result==1) {
-        // login the user if just created
-        include("login.php");
-      } else {
-        echo '
-          <font color="#FF0000"><b><i> Error. Please Try Again.</b></i></font>
-        ';
-      }
+        if ($result==1) {
+           // login the user if just created
+           include("login.php");
+        }
+        else {
+          echo '
+          <p>Error. Please Try Again.</p>
+          ';
+        }
                
-      mysqli_close($conn);
-    
-  } else {
-    /echo '
-      <p>Username already exists. Please log-in. Redirecting you home. <p/>
-      <meta http-equiv="refresh" content="3; url=index.html" />
-    ';
-  }
- 
+        mysqli_close($conn);
+        }
+     else {
+          echo '
+          <p>Username already exists. Please log-in. Redirecting you home. </p>
+          <meta http-equiv="refresh" content="3; url=index.html" />
+          ';
+     }
+?> 
 
-echo '
 </body>
 </html>
-';
-
-?>
