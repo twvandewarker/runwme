06316cf5987140902627d7a79d63687afe1c2c65
Travis Vandewarker <twvandewarker@gmail.com>
2019-05-09 21:23:11 -0400

    test addmember

--- connectDb.php
+++ connectDb.php
@@ -4,8 +4,12 @@
   $dbhost = "localhost";
   $dbuser = "twv_travis";     // CHANGE IT TO YOUR DATABASE USER NAME
   $dbpass = "twv_4rwZpa45";    // CHANGE IT TO YOUR DATABASE PASSWORD
-  
+
+echo "Set connection variables";
+
   $conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die ("Error connecting to mysql");
+
+echo "Connected";
 
   $dbname = "twv_runwme";     // CHANGE IT TO YOUR DATABASE NAME
 
